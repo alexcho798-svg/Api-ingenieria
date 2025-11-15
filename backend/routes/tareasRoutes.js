cb376efb2289f725f59877c67802c12743fff292
alexcho798-svg <alexcho798@gmail.com>
2025-11-15 17:06:12 -0600

    Update tareasRoutes.js

--- backend/routes/tareasRoutes.js
+++ backend/routes/tareasRoutes.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const router = express.Router()
-const {getTareas, createTareas, updateTareas, deleteTareas} = require('../controllers/tareasControllers')
+const {getTareas, createTareas, updateTareas, deleteTareas} = require('../controllers/tareascontrollers')
 const {protect}=require('../middleware/authmiddleware')
 
 router.get('/', protect, getTareas )
