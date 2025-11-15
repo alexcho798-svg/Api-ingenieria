fdb0801313d5b654ff0d34780e6cdabd944624ad
alexcho798-svg <alexcho798@gmail.com>
2025-11-15 17:30:56 -0600

    Update usersRoutes.js

--- backend/routes/usersRoutes.js
+++ backend/routes/usersRoutes.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const router = express.Router()
-const {login, register, data} = require('../controllers/userscontrollers')
+const {login, register, data} = require('../controllers/usersControllers')
 const {protect} = require ('../middleware/authmiddleware')
 
 //endpoints publicos
